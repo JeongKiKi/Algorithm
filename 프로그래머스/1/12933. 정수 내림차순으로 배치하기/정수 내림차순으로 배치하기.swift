324e9fa2ca7ce4ebc34b8d711ac0474042307015
JeongKiKi <125369115+JeongKiKi@users.noreply.github.com>
2023-11-30 22:54:31 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.19 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.swift
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.swift
@@ -1,11 +1,2 @@
 func solution(_ n:Int64) -> Int64 {
-       let digits = String(n).compactMap { Int(String($0)) }
-    
-    // 각 자릿수를 큰 순서로 정렬
-    let sortedDigits = digits.sorted(by: >)
-    
-    // 정렬된 자릿수를 다시 정수로 변환
-    let sortedNumber = Int64(sortedDigits.map { String($0) }.joined()) ?? 0
-    
-    return sortedNumber
-}
+  return Int64(String(Array(String(n)).sorted { $0 > $1 }))!}
