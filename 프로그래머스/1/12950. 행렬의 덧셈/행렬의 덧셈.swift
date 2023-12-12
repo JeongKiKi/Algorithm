cdff554f454e20b4ebb3786a90c50c2e4302ecc5
JeongKiKi <125369115+JeongKiKi@users.noreply.github.com>
2023-12-12 18:20:50 +0900

    [level 1] Title: 행렬의 덧셈, Time: 14.64 ms, Memory: 62.5 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.swift
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.swift
@@ -1,17 +1,9 @@
 func solution(_ arr1:[[Int]], _ arr2:[[Int]]) -> [[Int]] {
-    // 행렬의 행과 열의 크기 확인
-    let rows = arr1.count
-    let cols = arr1[0].count
-    
-    // 결과를 저장할 빈 행렬 생성
-    var result: [[Int]] = Array(repeating: Array(repeating: 0, count: cols), count: rows)
-    
-    // 행렬 덧셈 수행
-    for i in 0..<rows {
-        for j in 0..<cols {
-            result[i][j] = arr1[i][j] + arr2[i][j]
+    var a: [[Int]] = arr1
+    for i in 0..<arr1.count{
+        for k in 0..<arr1[i].count{
+            a[i][k] = arr1[i][k] + arr2[i][k]
         }
     }
-
-    return result
+    return a
 }
