f2f0e890dded309fbbfb8d037247e5a32261702f
JeongKiKi <125369115+JeongKiKi@users.noreply.github.com>
2024-01-10 16:34:08 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.25 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.swift
@@ -0,0 +1,18 @@
+func solution(_ s:String) -> String {
+    // var a = s.split(separator: " ")
+    let a = s.components(separatedBy: " ")
+    var b = ""
+    for i in a {
+        var c = ""
+        for (index, numbers) in i.enumerated() {
+           if index % 2 == 0 {
+               c += numbers.uppercased()
+           }else{
+               c += numbers.lowercased()
+           }
+        }
+        b += c + " "
+    }
+    var k = String(b.dropLast())
+    return k
+}
