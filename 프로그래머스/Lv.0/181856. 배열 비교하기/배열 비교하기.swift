daf92656bb3cbf3d63a4799d5bb0a0b99c312f23
JeongKiKi <125369115+JeongKiKi@users.noreply.github.com>
2024-01-15 19:55:31 +0900

    [level Lv.0] Title: 배열 비교하기, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181856. 배열 비교하기/배열 비교하기.swift
+++ 프로그래머스/Lv.0/181856. 배열 비교하기/배열 비교하기.swift
@@ -6,8 +6,8 @@ func solution(_ arr1:[Int], _ arr2:[Int]) -> Int {
     }else if arr1.count < arr2.count {
         return -1
     }else {
-        var arr1sum = arr1.reduce(0){$0 + $1}
-        var arr2sum = arr2.reduce(0){$0 + $1}
+        var arr1sum = arr1.reduce(0 , +)
+        var arr2sum = arr2.reduce(0, +)
         if arr1sum > arr2sum {
             return 1
         }else if arr1sum < arr2sum {
