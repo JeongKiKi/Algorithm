393e42a6745b2f27a9213d5b4ba3689b8cdaebb9
JeongKiKi <125369115+JeongKiKi@users.noreply.github.com>
2024-01-26 12:12:07 +0900

    [level 1] Title: 콜라 문제, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.swift
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.swift
@@ -1,15 +1,5 @@
 import Foundation
 
 func solution(_ a:Int, _ b:Int, _ n:Int) -> Int {
-    var binB = n
-    var h = 0
-    while binB >= a {
-        let k = binB / a //교환한 콜라수
-        let p = k * b 
-         let l = binB % a //교환 하지 못하고 남은 콜라병 수
-        h += p
-        binB = p + l
-        
-    }
-    return h
+     return (n > b ? n - b : 0) / (a - b) * b
 }
