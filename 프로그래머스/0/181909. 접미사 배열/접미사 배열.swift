3658c413787920b5579a9651802cc75c9c5cefb4
JeongKiKi <125369115+JeongKiKi@users.noreply.github.com>
2024-02-06 18:58:00 +0900

    [level 0] Title: 접미사 배열, Time: 0.12 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func solution(_ my_string:String) -> [String] {
+    var suffixes = [String]()
+
+    for i in 0..<my_string.count {
+        let startIndex = my_string.index(my_string.startIndex, offsetBy: i)
+        let suffix = String(my_string[startIndex...])
+        suffixes.append(suffix)
+    }
+
+    return suffixes.sorted()
+}
