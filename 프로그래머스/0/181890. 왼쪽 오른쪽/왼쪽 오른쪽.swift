82ca7044120927953d80920dbdbd3a9463bcbe99
JeongKiKi <125369115+JeongKiKi@users.noreply.github.com>
2024-03-18 19:02:19 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.05 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+func solution(_ str_list:[String]) -> [String] {
+    for i in 0..<str_list.count {
+        if str_list[i] == "l" {
+            return Array(str_list[0..<i])
+        }else if str_list[i] == "r"{
+            var a = i + 1
+            return Array(str_list[a...])
+        }
+    }
+    if !str_list.contains("l") || !str_list.contains("r"){
+        return []
+    }
+    return []
+}
