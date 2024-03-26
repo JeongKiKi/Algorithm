fe4b26bd89901f82da50716b1fc4f779eac1a3bf
JeongKiKi <125369115+JeongKiKi@users.noreply.github.com>
2024-03-26 11:51:56 +0900

    [level 0] Title: 로그인 성공?, Time: 0.03 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+func solution(_ id_pw:[String], _ db:[[String]]) -> String {
+    for userInfo in db {
+        if userInfo[0] == id_pw[0] {
+            if userInfo[1] == id_pw[1] {
+                return "login"
+            } else {
+                return "wrong pw"
+            }
+        }
+    }
+    return "fail"
+}
