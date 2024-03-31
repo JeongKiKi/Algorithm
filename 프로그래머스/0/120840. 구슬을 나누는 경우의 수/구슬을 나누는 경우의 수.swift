b1756882aecc5c85e0bac20c184ad84e1a3f2603
JeongKiKi <125369115+JeongKiKi@users.noreply.github.com>
2024-03-31 23:59:39 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.swift
+++ 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.swift
@@ -1,26 +1,25 @@
 import Foundation
 
 func solution(_ balls:Int, _ share:Int) -> Int {
-   if balls <= share || balls == 1 {
+    var a:Double = 1 
+ 
+    var c = share + 1 
+    var b:Double = 1
+     if balls <= share || balls == 1 {
         return 1
     }
 
-    // 2차원 배열 dp를 생성합니다. 초기값은 0으로 설정합니다.
-    var dp = Array(repeating: Array(repeating: 0, count: share + 1), count: balls + 1)
-
-    // dp 배열을 채웁니다.
-    for i in 0...balls {
-        for j in 0...min(i, share) {
-            if j == 0 || j == i {
-                // 경계 조건: j가 0이거나 j가 i인 경우에는 경우의 수가 1입니다.
-                dp[i][j] = 1
-            } else {
-                // 점화식을 사용하여 경우의 수를 구합니다.
-                dp[i][j] = dp[i-1][j] + dp[i-1][j-1]
-            }
+    if balls > share {
+        for i in c...balls {
+            a *= Double(i)
         }
+        var nm = balls - share
+        for k in 1...nm {
+            b *= Double(k)
     }
-
-    // dp[balls][share]를 반환합니다.
-    return dp[balls][share]
+    }else if balls == share {
+        return 1
+    }
+   
+    return Int(a / b)
 }
