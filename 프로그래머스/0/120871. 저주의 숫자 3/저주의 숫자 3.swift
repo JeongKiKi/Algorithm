8a56078ebe6e594896cf9969061a0bd2ff33e334
JeongKiKi <125369115+JeongKiKi@users.noreply.github.com>
2024-04-01 13:58:51 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.07 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.swift
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.swift
@@ -4,11 +4,9 @@ func solution(_ n:Int) -> Int {
     var b = 0 
     for i in 1...n {
         b += 1
-        if b % 3 == 0 || String(b).contains("3"){
-            while b % 3 == 0 || String(b).contains("3") {
+         while b % 3 == 0 || String(b).contains("3") {
                 b += 1
             }
-        }
     }
     return b
 }
